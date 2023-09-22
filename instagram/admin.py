37d51dfab458c3065ba120df575649f52739f8ee
Joey <hanazzang999@gmail.com>
2023-09-22 15:14:18 +0900

    instagram: admin 커스텀 (Post)

--- instagram/admin.py
+++ instagram/admin.py
@@ -5,4 +5,7 @@ from .models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['message','created_at']
+    list_display = ['id','is_public','message','message_length','created_at']
+    list_display_links = ['message']
+    list_filter = ['created_at','is_public']
+    search_fields = ['message']
--- instagram/models.py
+++ instagram/models.py
@@ -5,3 +5,11 @@ class Post(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+
+    def __str__(self):
+        return f"instagram {self.message}"
+
+    def message_length(self):
+        return len(self.message)
+
