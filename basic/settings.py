b17596050c72a3170799f7c797f8bf19f91cfb40
Joey <hanazzang999@gmail.com>
2023-09-25 23:27:31 +0900

    instagram: view & template (post_list)

--- basic/settings.py
+++ basic/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'instagram',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- instagram/models.py
+++ instagram/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message = models.TextField()
-    photo = models.ImageField(blank=True)
+    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y%m')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
@@ -13,4 +13,7 @@ class Post(models.Model):
 
     def message_length(self):
         return len(self.message)
+
+    class Meta:
+        ordering = ['-id']
 
