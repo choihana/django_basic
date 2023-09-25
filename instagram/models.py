0a26426c23d5bace41f9c8f1cc1507de87e36428
Joey <hanazzang999@gmail.com>
2023-09-26 00:01:42 +0900

    instagram: model 추가 (comment)

--- instagram/models.py
+++ instagram/models.py
@@ -17,3 +17,12 @@ class Post(models.Model):
     class Meta:
         ordering = ['-id']
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'message:{self.message}'
+
