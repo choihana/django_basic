914c9512937321e4ed5b12e83e85a960e7c63d4f
Joey <hanazzang999@gmail.com>
2023-09-25 23:54:58 +0900

    debug toolbar setting

--- basic/settings.py
+++ basic/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'blog',
     'instagram',
     'django_extensions',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS =['127.0.0.1', ]
--- basic/urls.py
+++ basic/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('instagram/', include('instagram.urls')),
+    path('__debug__', include('debug_toolbar.urls')),
 ]
 
 
