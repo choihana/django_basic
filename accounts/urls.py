59da0523dad8e23156dba52bcb98841c903ca76a
Joey <hanazzang999@gmail.com>
2023-09-27 10:41:31 +0900

    profile, login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from . import views
+
 
 urlpatterns=[
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit', views.profile_edit, name='profile-edit'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/' , LogoutView.as_view(), name='logout'),
+
 ]
